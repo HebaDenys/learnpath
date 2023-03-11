f16cc2fc663542bc84ddbd249cb70388168c55b3
Denys Heba <denys.heba@thinkopen.it>
2023-03-11 19:53:04 +0100

    added base files to compile the main and the test

---
+++ src/main/java/com/learnpath/spring/mongo/SpringAndMongoLearningApplication.java
@@ -0,0 +1,13 @@
+package com.learnpath.spring.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndMongoLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndMongoLearningApplication.class, args);
+	}
+
+}
